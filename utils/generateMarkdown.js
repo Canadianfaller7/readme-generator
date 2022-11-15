f3d215b5363aca7ad6ec79ca3ec5542373bdafd9
Spencer Merrill <canadianfaller7@gmail.com>
2022-11-14 23:10:01 -0700

    trying to return badge in the license section of the readme

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,13 +3,14 @@
 // function renderLicenseBadge(license) {}
 const renderLicenseBadge = license => {
   if (license === 'MIT') {
-    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+    let badge;
+    return badge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
   }
   else if (license === 'Apache License 2.0') {
-    return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+    return badge = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
   }
   else if (license === 'GNU General Public License v3.0') {
-    return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+    return badge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
   }
   else {
     return ''
@@ -21,24 +22,22 @@ const renderLicenseBadge = license => {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 // function renderLicenseSection(license) {}
-const renderLicenseSection = license => {
+const renderLicenseSection = (license, badge) => {
   if (license === 'No License') {
     return ''
   }
   else {
-    return `
-    ## License
-    - ${license}, Please refer to the LICENSE in the repo.`
+    return `## License
+    - ${license} ${badge}, Please refer to license page by clicking on the badge to learn more.`
   }
 }
 
 // TODO: Create a function to generate markdown for README
 const writeToFile = ({ projectTitle, description, description2, description3, description4, installation, usage, credits, thirdParty, tutorials, license, liveRepo, screenshotName1, screenshotName2, screenshotName3, imageName1, imageName2, imageName3, github, email }) =>
   `# ${projectTitle}
-  ${renderLicenseBadge(license)}
+${renderLicenseBadge(license)}
 
 ## Description
-
 - ${description}
 - ${description2}
 - ${description3}
@@ -66,7 +65,6 @@ If your README is long, add a table of contents to make it easy for users to fin
 - ${credits}
 - ${thirdParty}
 - ${tutorials}
-
 
 ${renderLicenseSection(license)}
 
